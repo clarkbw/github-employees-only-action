4798e5d2de09ce98dbf42084f59a5c21f7394985
Bryan Clark <clarkbw@github.com>
2019-08-13 17:04:45 -0700

    better debugging and prep for acutally removing

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,10 @@ async function run() {
     core.debug(`checking team #${team_slug}`);
     const octokit = new github.GitHub(token);
 
-    const { owner: org } = github.context.repo;
-    const {
-      data: { id: team_id }
-    } = await octokit.teams.getByName({
-      org,
-      team_slug
-    });
-    core.debug(`team_id ${team_id}`);
+    const { owner } = github.context.repo;
     const query = `query {
-      organization(login:"maintainers") {
-        teams(query:"github-employees", first:1){
+      organization(login:"${owner}") {
+        teams(query:"${team_slug}", first:1){
           nodes {
             name,
             id,
@@ -32,26 +25,28 @@ async function run() {
         }
       }
     }`;
-    function kenny(logins) {
-      return logins.map(a => a.login).join(",");
-    }
     const results = await octokit.graphql(query);
-    core.debug(`STRINGIFY ${JSON.stringify(results)}`);
+    core.debug(`results ${JSON.stringify(results)}`);
     const members = results.organization.teams.nodes[0].members.nodes;
-    core.debug(`members ${kenny(members)}`);
+    core.debug(`members ${JSON.stringify(members)}`);
     const alumni = members.filter(member => !member.isEmployee);
-    core.debug(`alumni ${kenny(alumni)}`);
-    // members.forEach(member => {
-    //   if (member.isEmployee) {
-    //     core.debug(`employee! ${member}`);
-    //   } else {
-    //     core.debug(`NOT AN EMPLOYEE! ${member}`);
-    //     // octokit.teams.removeMembership({
-    //     //   team_id: id,
-    //     //   username: member.login
-    //     // });
-    //   }
-    // });
+    core.debug(`alumni ${JSON.stringify(alumni)}`);
+    // if (alumni.length > 0) {
+    //   const {
+    //     data: { id: team_id }
+    //   } = await octokit.teams.getByName({
+    //     org: owner,
+    //     team_slug
+    //   });
+    //   core.debug(`team_id ${team_id}`);
+    //   alumni.forEach(alum => {
+    //     core.debug(`NOT AN EMPLOYEE! ${JSON.stringify(alum)}`);
+    //     octokit.teams.removeMembership({
+    //       team_id,
+    //       username: alum.login
+    //     });
+    //   });
+    // }
   } catch (error) {
     core.setFailed(error.message);
   }
